a7a3647da02974959390ae705d6d77d69059093a
oshaleyko59 <oshaleyko@yahoo.com>
2023-05-10 11:09:14 -0500

    cosmetic

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,20 @@
-import React, { Component } from "react";
-import { Searchbar } from "components/Searchbar";
-import { Loader } from "components/Loader";
-import { ImageGallery } from "components/ImageGallery";
-import { Button } from "components/Button";
-import { Modal } from "components/Modal";
+import React, { Component } from 'react';
+import { Searchbar } from 'components/Searchbar';
+import { Loader } from 'components/Loader';
+import { ImageGallery } from 'components/ImageGallery';
+import { Button } from 'components/Button';
+import { Modal } from 'components/Modal';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '34354897-5c7498590c159fab15c37271f';
-const searchParams = new URLSearchParams({  //TODO:
-  q: "cat",
+const searchParams = new URLSearchParams({
+  //TODO:
+  q: 'cat',
   page: 1, //default
   key: API_KEY,
-  image_type: "photo",
-  orientation: "horizontal",
-  per_page:12,
+  image_type: 'photo',
+  orientation: 'horizontal',
+  per_page: 12,
 });
 
 //https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12
@@ -27,21 +28,39 @@ const searchParams = new URLSearchParams({  //TODO:
 export class App extends Component {
   state = {
     isModalVisible: false,
-    gallery:[],
-  }
+    isLoading: false,
+    query: '',
+    gallery: null,
+  };
 
-  onSubmit = () => {
-//TODO:
-  }
+  handleSubmit = (query) => {
+    console.log(query);  //TODO:
+    this.setState({ query });
+  };
 
+  componentDidMount() {
+    this.setState({ isLoading: true });
+
+    fetch(
+      `https://pixabay.com/api/?q=cat&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    )
+      .then(res => res.json())
+      .then(gallery => this.setState({ gallery }))
+      .catch(console.log).finally(()=> this.setState({isLoading: false}));
+  }
+  /*  <ImageGallery gallery={this.state.gallery} /> */
   render() {
     return (
       <>
-        <Searchbar onSubmit={this.onSubmit} />
-        <Loader isVisible={true} />
-        <ImageGallery gallery={this.state.gallery}></ImageGallery>
+        <Searchbar onSubmit={this.handleSubmit} />
+        {this.state.isLoading && <Loader isVisible={true} />}
+        {this.state.gallery && (
+          <div> Galereya</div>
+
+        )}
         <Button />
         {this.state.isModalVisible && <Modal />}
-      </>)
+      </>
+    );
   }
-};
+}
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,25 +1,55 @@
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Header, Form, Button, Span, Input } from './styled';
+//TODO: import {ImSearch } from 'react-icons/im';
+//TODO: import { ReactComponents as MyIcon } from './some.svg';
 
 /* Компонент приймає один проп onSubmit – функцію для передачі
 значення інпута під час сабміту форми */
 
-export const Searchbar = ({ onSubmit }) => (
-  <Header>
-    <Form>
-      <Button type="submit" onSubmit={onSubmit}>
-        <Span>Search</Span>
-      </Button>
-      <Input
-        type="text"
-        autocomplete="off"
-        autoFocus
-        placeholder="Search images and photos"
-      />
-    </Form>
-  </Header>
-);
+export class Searchbar extends Component {
 
-Searchbar.propTypes = {
-  onSubmit: PropTypes.func
+  static propTypes = {
+    onSubmit: PropTypes.func,
+  };
+
+  state = {
+    query: ''
+  };
+
+  handleQueryChange = (e) => {
+    this.setState({ query: e.currentTarget.value.toLowerCase() });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.setState.query);
+    this.setState({ query: '' });
+  }
+
+  render() {
+    return (
+      <Header>
+        <Form>
+          <Button type="submit" onSubmit={this.handleQueryChange}>
+            <Span>Search</Span>
+          </Button>
+          <Input
+            type="text"
+            name="query"
+            value={this.state.query}
+            onChange={this.handleQueryChange}
+            autocomplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </Form>
+      </Header>
+    );
+  }
 }
+
+/* Searchbar.propTypes = {
+  onSubmit: PropTypes.func,
+};
+ */
