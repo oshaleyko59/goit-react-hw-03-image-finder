b414386d489b44f6b7d5457b2485836203d6bac8
oshaleyko59 <oshaleyko@yahoo.com>
2023-05-16 02:32:52 -0500

    cosmetic

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,10 +1,8 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Loader } from 'components/Loader';
 import { Button } from 'components/Button';
 import { ImageGalleryItem, Gallery, Img } from './styled';
-//import { toast } from 'react-toastify';
-//import galleryApi from 'services/galleryAPI';
 import galleryApi from 'services/galleryFetch';
 
 /*
@@ -38,40 +36,46 @@ export class ImageGallery extends Component {
     totalHits: 0,
   };
 
+  constructor(props) {
+    super(props);
+    this.listRef = React.createRef();
+  }
+
   componentDidUpdate(prevProps, prevState) {
     const { query } = this.props;
-    const { page } = this.state;
+    const { page, status } = this.state;
+    if (status === STATUS.PENDING) return;
 
     if (prevProps.query !== query) {
       //new query
-      this.getPictures(1);
+      this.addPictures(1);
       return;
     }
 
     if (prevState.page !== page) {
       //next page
-      this.getPictures(page);
+      this.addPictures(page);
     }
   }
 
-  getPictures(page) {
+  addPictures(page) {
+    let newState = { status: STATUS.PENDING, page };
+    if (page === 1) newState.gallery = [];
+    this.setState(newState);
+
     const { query } = this.props;
-    const gallery  = page === 1 ? [] : this.state.gallery;
-
-    this.setState({ status: STATUS.PENDING, page });
-
     galleryApi
       .fetchGallery(query, page)
-      .then(({hits, totalHits}) => {
+      .then(({ hits, totalHits }) => {
         if (!hits.length) {
           return Promise.reject(new Error(`Nothing found for "${query}`));
         }
 
-        this.setState({
-          gallery: [...gallery, ...hits],
+        this.setState(state => ({
+          gallery: [...state.gallery, ...hits],
           totalHits,
           status: STATUS.RESOLVED,
-        });
+        }));
       })
       .catch(error => this.setState({ error, status: STATUS.REJECTED }));
   }
@@ -85,58 +89,34 @@ export class ImageGallery extends Component {
     this.setState(({ page }) => ({ page: page + 1 }));
   };
 
-  errorView() {
-    // toast.error(this.state.error.message); TODO:
-    return <h3>{this.state.error.message}"</h3>;
-  }
-
-  idleView() {
-    return <></>;
-  }
-
-  galleryView() {
-    const {
-      gallery,
-      totalHits,
-    } = this.state;
-    console.log('totalHits, loaded:', totalHits, gallery.length); //TODO:
-    return (
-      <>
-        <Gallery>
-          {gallery.map(({ id, webformatURL, tags }, index) => {
-            return (
-              <ImageGalleryItem
-                key={id}
-                onClick={() => {
-                  this.handleClickItem(index);
-                }}
-              >
-                <Img src={webformatURL} alt={tags} />
-              </ImageGalleryItem>
-            );
-          })}
-        </Gallery>
-        {gallery.length < totalHits && <Button loadMore={this.loadMore} />}
-      </>
-    );
-  }
-
   render() {
     const { query } = this.props;
-    const { status } = this.state;
+    const { status, gallery, totalHits } = this.state;
 
-    switch (status) {
-      case STATUS.PENDING:
-        return <Loader query={query} />;
-
-      case STATUS.RESOLVED:
-        return this.galleryView();
-
-      case STATUS.REJECTED:
-        return this.errorView();
-
-      default:
-        return this.idleView();
-    }
+    return (
+      <div ref={this.listRef}>
+        {status === STATUS.REJECTED && <h3>{this.state.error.message}"</h3>}
+        {status !== STATUS.IDLE && (
+          <Gallery>
+            {gallery.map(({ id, webformatURL, tags }, index) => {
+              return (
+                <ImageGalleryItem
+                  key={id}
+                  onClick={() => {
+                    this.handleClickItem(index);
+                  }}
+                >
+                  <Img src={webformatURL} alt={tags} loading="lazy" />
+                </ImageGalleryItem>
+              );
+            })}
+          </Gallery>
+        )}
+        {status === STATUS.PENDING && <Loader query={query} />}
+        {status === STATUS.RESOLVED && gallery.length < totalHits && (
+          <Button loadMore={this.loadMore} />
+        )}
+      </div>
+    );
   }
 }
